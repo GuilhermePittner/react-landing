dbd5ac6bfc82229a5255c0c76dc575e5da1a7046
guilherme_pittner <guilherme_pittner@ethercity.tech>
2025-02-11 22:27:31 -0300

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Features from "./components/Features"
+import Workflow from "./components/Workflow"
+import Pricing from "./components/Pricing"
+import Testimonials from "./components/Testimonials"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -11,7 +15,12 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
         <Features />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
       </div>
+
     </>
   )
 }
